11570ecbc8fec6cdaf54ecdda169bdbc738dddd7
robertozh29 <robertozh29@hotmail.com>
2022-02-11 17:33:20 -0600

    basic structure

--- send.php
+++ send.php
@@ -1,9 +1,18 @@
 <?php 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$info = $_POST['info'];
 
-echo $name;
+if(isset($_POST["submit"])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $info = $_POST['info'];
+
+    // Mail Header
+    $mailTo = "contacto@lacasadelasflores.rule.com.mx";
+    $subject = "New concact from la casa de las flores"; 
+    $txt = "You have recieve a email from ". $name . "\n\n" . $info;
+    $from = "From: ". $email;
+ 
+    mail($mailTo, $subject, $txt, $from);
+}
 
 
 ?>
